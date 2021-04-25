a586a7b7b0d90496c41783c89f3594041d75725a
anonymous4747 <72137891+anonymous4747@users.noreply.github.com>
2021-04-25 16:43:46 +0530

    Update verify.py

--- AccGenBot/verify.py
+++ AccGenBot/verify.py
@@ -17,3 +17,18 @@ async def verify(channel_id, event, bot):
                 return True
     except telethon.errors.rpcerrorlist.UserNotParticipantError:
         return False
+
+from telethon import Button, custom, events, functions
+import telethon
+
+async def verify2(channel_id, event, bot):
+    try:
+            result = await bot(
+                functions.channels.GetParticipantRequest(
+                    channel='@DynamicCoderZ', user_id=event.sender_id
+                )
+            )
+            if result.participant:
+                return True
+    except telethon.errors.rpcerrorlist.UserNotParticipantError:
+        return False
